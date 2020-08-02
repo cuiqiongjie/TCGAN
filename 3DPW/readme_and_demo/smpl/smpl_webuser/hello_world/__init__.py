0677af61e651f75de01a543c7d176366016c93e5
cui <cuiqiongjie@126.com>
2020-08-02 17:13:40 +0800

    code and data

---
+++ 3DPW/readme_and_demo/smpl/smpl_webuser/hello_world/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@IDE: PyCharm
+@author: Cui
+@contact: cuiqiongjie@126.com
+@time: 2019,5月
+Copyright (c),Nanjing University of Science and Technology
+
+@Desc: 
+
+"""
